59dece688c47f10b6388d0ef0f0b732f424ecad8
ogasawara.yuka <ogasawara.yuka@relation2012.com>
2023-08-01 15:31:30 +0900

    8.6章まで

--- app/blog/schedule/page.tsx
+++ app/blog/schedule/page.tsx
@@ -1,11 +1,51 @@
 import { getPostBySlug } from "lib/api";
+import { extractText } from "lib/extract-text";
 import Container from "components/container";
+import PostHeader from "components/post-header";
+import PostBody from "@/components/post-body";
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from "components/two-column";
+import ConvertBody from "components/convert-body";
+import PostCategories from "components/post-categories";
+import Image from "next/image";
 
 export default async function Shedule() {
   const data = await getData();
   return (
     <Container>
-      <h1>{data.props.title}</h1>
+      <article>
+        <PostHeader
+          title={data.props.title}
+          subtitle="Blog Article"
+          publish={data.props.publish}
+        />
+
+        <figure>
+          <Image
+            src={data.props.eyecatch.url}
+            alt=""
+            layout="responsive"
+            width={data.props.eyecatch.width}
+            height={data.props.eyecatch.height}
+            sizes="(min-width: 1152px) 1152px, 100vw"
+            priority
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={data.props.content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={data.props.categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
     </Container>
   );
 }
@@ -14,6 +54,7 @@ export async function getData() {
   const slug = "schedule";
 
   const post = await getPostBySlug(slug);
+  const description = extractText(post.content);
 
   return {
     props: {
@@ -22,6 +63,7 @@ export async function getData() {
       content: post.content,
       eyecatch: post.eyecatch,
       categories: post.categories,
+      description: description,
     },
   };
 }
